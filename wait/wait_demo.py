18ee08b806b4d679ea1b7c18399827d25755001f
daniel <daniel.lin.gr@gmail.com>
2021-06-28 17:04:36 +0800

    Create wait_demo.py

---
+++ wait/wait_demo.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+driver = webdriver.Firefox()
+
+driver.implicitly_wait(30)  # 隐性等待，最长等30秒
+driver.get('https://your.url')
+print driver.current_url
+driver.quit()
+
+
+# 顯示等待
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.expected_conditions import visibility_of_element_located
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# 等待结果加载
+WebDriverWait(driver, 60).until(visibility_of_element_located((By.ID, 'grView')))
+
+driver.get('https://www.baidu.com/')
+baidu_input = (By.ID, 'kw')
+WebDriverWait(driver,10).until(EC.visibility_of_element_located(baidu_input))
+
