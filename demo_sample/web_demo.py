e8f7dbcf0e5e299e6707ebe673686f146f5612fb
daniel <daniel.lin.gr@gmail.com>
2021-11-23 13:40:45 +0800

    Create web_demo.py

---
+++ demo_sample/web_demo.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+from time import sleep
+
+
+with webdriver.Firefox() as driver:
+	driver.get("http://baidu.com")
+	sleep(5)
+	
