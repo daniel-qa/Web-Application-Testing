fdb8f50a839722803301cb05b73eb96d15978c73
daniel <daniel.lin.gr@gmail.com>
2022-05-04 16:15:30 +0800

    Create web_sample1.spec.js

---
+++ Cypress/Sample/web_sample1.spec.js
@@ -0,0 +1,18 @@
+// Strat Cypress 
+// npx cypress open
+
+//Step 1: Login 
+describe('入校老師新建個人班級', () => {
+  it('Login', () => {
+    cy.visit('https://www.teammodel.net')
+	cy.get(':nth-child(2) > .login-box-cont > .main > .subtitle').click()
+	cy.get('#tmdID').type('guest')
+	cy.get('#tmdpw').type('xxxx')
+	cy.get('.iconFrame').click()
+  })
+  
+  it('Visits 學生管理', () => {
+    cy.visit('https://www.teammodel.net/home/studentAccount/StuMgt')
+  	cy.get(':nth-child(2) > .login-box-cont > .main > .subtitle').click()
+  })
+})
