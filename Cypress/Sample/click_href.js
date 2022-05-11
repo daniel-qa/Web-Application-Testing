70970abf6090ad2732a3fe4699caf2347fcdacda
daniel <daniel.lin.gr@gmail.com>
2022-05-11 18:03:13 +0800

    Create click_href.js

---
+++ Cypress/Sample/click_href.js
@@ -0,0 +1,13 @@
+#<div class="column">
+#    <a href="/admin/contents/109">
+#        <div class="ui segment">
+#            <div class="ui center aligned header">
+#                Content 6
+#                <div class="sub header">Add description to content packages
+#                </div>
+#            </div>
+#        </div>
+#    </a>
+#</div>
+
+cy.get('a[href*="admin/contents"]').contains('Content 6').click()
